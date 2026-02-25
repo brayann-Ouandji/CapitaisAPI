105653c1ec4ef4512a379c6458b115210609ed43
PENWE <stephe.penwe@groupe-esigelec.org>
2026-02-25 09:04:59 +0100

    Authentification pour acceder aux ressouces de countryservlet

--- src/main/java/fr/esigelec/api/jee/CountryServlet.java
+++ src/main/java/fr/esigelec/api/jee/CountryServlet.java
@@ -5,6 +5,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -16,45 +18,58 @@ import fr.esigelec.api.dao.DBDAO;
 @WebServlet("/CountryServlet")
 public class CountryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CountryServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public CountryServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+		HttpSession session = request.getSession(false);
+		if (session == null  || session.getAttribute("UserName") == null) {
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			response.setContentType("application/json");
+			PrintWriter out = response.getWriter();
+			out.print("{\"Message\" : \"The authentification is required to access to a session\"}");
+			return;
+		}
 		String countrycode = request.getParameter("countrycode");
 		DBDAO dbdao = new DBDAO();
 		CountryModel countrym = dbdao.getCountry(countrycode);
 		if (countrycode == null || countrycode.isEmpty()) {
 			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 			return;
-		} 
-		
+		}
+
 		if (countrym == null) {
 			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 			return;
-			
-		} 
-			response.setContentType("application/json");
-			PrintWriter out = response.getWriter();
-			String jsonResponse = "{\"country\": \"" + countrym.getName() + "\", \"capital\": \"" + countrym.getCapital() + "\", \"ccode\": \"" + countrym.getCode() + "\"}";
-			out.print(jsonResponse);
-			out.flush();
+
+		}
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		String jsonResponse = "{\"country\": \"" + countrym.getName() + "\", \"capital\": \"" + countrym.getCapital()
+				+ "\", \"ccode\": \"" + countrym.getCode() + "\"}";
+		out.print(jsonResponse);
+		out.flush();
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
